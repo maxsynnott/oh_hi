c8fd3ac8200e60715da7b1733acb4eac020c0d7d
maxsynnott <maxryansynnott@gmail.com>
2020-03-24 13:39:19 +1300

    app to class and redux installed

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Row from './Row'
 import Timer from './Timer'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Timer/>
-      
-      <Row/>
-      <Row/>
-      <Row/>
-      <Row/>
-      <Row/>
-      <Row/>
-      <Row/>
-      <Row/>
-      <Row/>
-      <Row/>
-    </View>
-  );
+import store from './store'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Timer/>
+
+        <Row/>
+        <Row/>
+        <Row/>
+        <Row/>
+        <Row/>
+        <Row/>
+        <Row/>
+        <Row/>
+        <Row/>
+        <Row/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
